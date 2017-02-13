f75342c1f5a2b0d109483b2b1a8f282220154fb9
RobbyP <robprescott715@gmail.com>
2017-02-12 22:11:18 -0600

    1st Push
    dads

---
+++ OurGame/CS491/Assets/Sprites/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    int currentDir = 2;
+    Vector2 input;
+    public bool isMoving; 
+    float t;
+    public float walkSpeed;
+
+    Animator animator; 
+    float moveVelocityX;
+    float moveVelocityY;
+   
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        moveVelocityX = 0;
+        moveVelocityY = 0;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocityX, moveVelocityY);
+        isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+        animator.SetBool("isMoving", isMoving);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentDir = 0;
+            moveVelocityY = walkSpeed * 3;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentDir = 2;
+            moveVelocityY = -walkSpeed * 3;
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentDir = 3;
+            moveVelocityX = -walkSpeed * 4;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentDir = 1;
+            moveVelocityX = walkSpeed * 4;
+
+        }
+       
+        animator.SetInteger("walkDirectionAnimation", currentDir);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocityX, moveVelocityY) * Time.deltaTime;
+
+
+
+    }
+        
+}
+
+
