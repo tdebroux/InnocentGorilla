1d4e85070d34c60131c71089667bf60b36baea5a
RobbyP <robprescott715@gmail.com>
2017-03-05 01:58:53 -0600

    PUSHING YET AGAIN
    ...

---
+++ OurGame/CS491/Assets/Scripts/LayerSorterStationary.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSorterStationary : MonoBehaviour {
+    public int displacement;
+    // Use this for initialization
+    void Start () {
+        GetComponent<SpriteRenderer>().sortingOrder = (Mathf.RoundToInt((transform.position.y) * 100f) - displacement) * -1;
+    }
+}
