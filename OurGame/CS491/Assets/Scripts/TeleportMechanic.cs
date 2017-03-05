13bb0e622280cf0191a8f6e35249c1730f0d7170
RobbyP <robprescott715@gmail.com>
2017-03-04 18:22:13 -0600

    DoorToTheOutSideWorld
    First teleport mechanic introduced

---
+++ OurGame/CS491/Assets/Scripts/TeleportMechanic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+  
+public class TeleportMechanic : MonoBehaviour {
+   public float x;
+   public float y;
+   private Vector2 TeleportPosition;
+
+    // Use this for initialization
+    void Start () {
+    TeleportPosition = new Vector2(x, y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "MainCharacter")
+        {
+            other.transform.position = TeleportPosition;
+        }
+    }
+}
