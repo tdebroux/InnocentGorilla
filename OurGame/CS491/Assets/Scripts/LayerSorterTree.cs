00036038d59880bb2d81424a3988a90c5b392f8f
RobbyP <robprescott715@gmail.com>
2017-03-07 11:02:13 -0600

    BOO!
    boo!

--- OurGame/CS491/Assets/Scripts/LayerSorterTree.cs
+++ OurGame/CS491/Assets/Scripts/LayerSorterTree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LayerSorterTree : MonoBehaviour {
-
+   
 	// Use this for initialization
 	void Start () {
         GetComponent<SpriteRenderer>().sortingOrder = (Mathf.RoundToInt((transform.position.y) * 100f) - 100) * -1;
