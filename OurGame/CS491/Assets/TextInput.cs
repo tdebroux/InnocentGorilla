a50053f11991ff755cd0a426ce3fb4ba19aab01c
RobbyP <robprescott715@gmail.com>
2017-03-13 21:53:58 -0500

    gameOver Script

--- OurGame/CS491/Assets/TextInput.cs
+++ OurGame/CS491/Assets/TextInput.cs
@@ -3,36 +3,44 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class TextInput : MonoBehaviour {
-
 	InputField input;
 	InputField.SubmitEvent se;
 	public Text output;
 	private int lineCount = 0;
 	public Text correct;
+    Movement Player;
+    public bool gameOver;
 
 	void Start () {
+        gameOver = false;
+        Player = FindObjectOfType<Movement>();
 		input = gameObject.GetComponent<InputField> ();
 		se = new InputField.SubmitEvent ();
 		se.AddListener (SubmitInput);
 		input.onEndEdit = se;
-
 	}
 	private void SubmitInput(string arg0){
-		if (lineCount < 4) {
+		if (lineCount < 8) {
 			output.text += "\n" + arg0;
 			input.text = "";
 			lineCount++;
-			if(lineCount == 4){
+			if(lineCount == 8){
 				if (output.text.Trim().Equals (correct.text.Trim())) {
-					print ("Text matches.");
-					output.text = "\nJohn Smith \n18 \nMilwaukee, WI\n414-638-6382\n\n" + "Do these records match?";
+					output.text = "\nJohn Smith \n35 \n420 Marketplace Terr.\nMilwaukee, WI 53226\n414-638-6382\nyeahboi@blazeit.net\n$520,500.00\n10.5%\n" + "Do these records match?";
 				} else {
-					print ("Text doesn't match.");
 					output.text = output.text + "\n\n" + "Do these records match?";
 				}
+                if (Input.GetKeyDown(KeyCode.Return)){
+                    gameOver = true; 
+                }
 			}
 		} 
 		input.ActivateInputField ();
 	}
+
+    public bool getGameOver() {
+        return gameOver;
+    }
+
 
 }
