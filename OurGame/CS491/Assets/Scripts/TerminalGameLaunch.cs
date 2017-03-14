c3612bc6a9fbea3e8aa4020b6d979ac7907a2314
tdebroux <debroutp0968@uwec.edu>
2017-03-13 21:55:22 -0500

    TerminalScriptsAdded

---
+++ OurGame/CS491/Assets/Scripts/TerminalGameLaunch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalGameLaunch : MonoBehaviour
+{
+    public bool requireButtonPress;
+    private bool waitForPress;
+    public bool destroyWhenActivated;
+    Movement player;
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Movement>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "MainCharacter")
+        {
+            if (requireButtonPress)
+            {
+                waitForPress = true;
+                return;
+            }
+
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitForPress && Input.GetKeyDown(KeyCode.J))
+        {
+            //launch minigame
+            //check if game is done boolean
+            player.canMove = false;
+
+            player.canMove = true;
+            print("YO");
+        }
+    }
+}
