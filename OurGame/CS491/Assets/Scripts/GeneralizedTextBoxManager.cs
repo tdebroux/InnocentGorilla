da12c1f4059d6e2a2fbf1636806fd9ae1f37faed
RobbyP <robprescott715@gmail.com>
2017-03-28 04:29:52 -0500

    Boss on the grass
    Robbys test scene

---
+++ OurGame/CS491/Assets/Scripts/GeneralizedTextBoxManager.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralizedTextBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public Movement player;
+
+    public bool isActive;
+
+    public bool stopPlayerMovement;
+
+    private bool isTyping = false;
+    private bool cancelTyping = false;
+    public float typeSpeed;
+
+    Animator animator;
+    public int characterNum;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Movement>();
+        animator = GetComponent<Animator>();
+        animator.SetInteger("CharacterNumber", characterNum);
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+        {
+            DisableTextBox();
+        }
+
+    }
+
+    void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+
+        //theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isTyping)
+            {
+                currentLine += 1;
+                if (textLines[currentLine].Length > 7)
+                {
+                    if (checkSwitch())
+                    {
+                        //switch image sprite
+                        if (textLines[currentLine].Substring(9).Trim().Equals("Douglas"))
+                        {
+                            characterNum = 2;
+                        }
+                        else if (textLines[currentLine].Substring(9).Trim().Equals("Player"))
+                        {
+                            characterNum = 1;
+                        }
+                        currentLine++;
+                  
+                    }
+                    else if (moveEvent())
+                    {
+                        //trigger event in "MoveEventCommands" script
+                        currentLine++;
+                    }
+                    if (currentLine > endAtLine)
+                    {
+                        DisableTextBox();
+                    }
+                    else
+                    {
+                        StartCoroutine(TextScroll(textLines[currentLine]));
+                    }
+                    animator.SetInteger("CharacterNumber", characterNum);
+                }
+            }
+            else if (isTyping && !cancelTyping)
+            {
+                cancelTyping = true;
+            }
+        }
+    }
+    private IEnumerator TextScroll(string lineofText)
+    {
+        int letter = 0;
+        theText.text = "";
+        isTyping = true;
+        cancelTyping = false;
+        while (isTyping && !cancelTyping && (letter < lineofText.Length - 1))
+        {
+            theText.text += lineofText[letter];
+            letter += 1;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        theText.text = lineofText;
+        isTyping = false;
+        cancelTyping = false;
+    }
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        isActive = true;
+        if (stopPlayerMovement)
+        {
+            player.canMove = false;
+        }
+        if (currentLine < textLines.Length)
+        {
+            StartCoroutine(TextScroll(textLines[currentLine]));
+        }
+    }
+
+    public bool moveEvent()
+    {
+        return textLines[currentLine].Substring(0, 7).Equals("(event)");
+    }
+
+    public bool checkSwitch()
+    {
+        return textLines[currentLine].Substring(0, 8).Equals("(switch)");
+    }
+
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        isActive = false;
+        player.canMove = true;
+
+    }
+    public void ReloadScript(TextAsset theText)
+    {
+        if (theText != null)
+        {
+            textLines = new string[1];
+            textLines = (theText.text.Split('\n'));
+        }
+    }
+
+}
---
+++ OurGame/CS491/Assets/Scripts/MoveEventCommands.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEventCommands : MonoBehaviour
+{
+    // public rail movements - what the ding dong choose
+    public int dir0;
+    public float x0;
+    public float y0;
+
+    public int dir1;
+    public float x1;
+    public float y1;
+
+    public int dir2;
+    public float x2;
+    public float y2;
+
+    public int dir3;
+    public float x3;
+    public float y3;
+
+    public int dir4;
+    public float x4;
+    public float y4;
+
+    public int dir5;
+    public float x5;
+    public float y5;
+
+    public int dir6;
+    public float x6;
+    public float y6;
+
+    public int dir7;
+    public float x7;
+    public float y7;
+
+
+    float[] currentXs;
+    float[] currentYs;
+    int[] currentDir;
+
+
+
+
+    bool reachX1;
+    bool reachY1;
+
+
+    private bool isMoving;
+    public float walkSpeed;
+    Vector2 input;
+    Animator animator;
+    float moveVelocityX;
+    float moveVelocityY;
+    private bool canMove;
+    private int rndInt;
+    bool timesUp;
+    int i;
+    GeneralizedTextBoxManager tBoxM;
+
+    // Use this for initialization
+    void Start()
+    {
+        float[] currentXs = { x0, x1, x2, x3, x4, x5, x6, x7 };
+        float[] currentYs = { y0, y1, y2, y3, y4, y5, y6, y7 };
+        int[] currentDir = { dir0, dir1, dir2, dir3, dir4, dir5, dir6, dir7 };
+
+        tBoxM = FindObjectOfType<GeneralizedTextBoxManager>();
+        i = 0;
+        animator = GetComponent<Animator>();
+        canMove = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (tBoxM.moveEvent())
+        {
+            // Decide Walk or Turn
+            if (currentDir[i] == 0) // if it is zero, we're walkin'!
+            {
+                canMove = true;
+                //Decide X or Y
+                if(currentXs[i] != 0)
+                {
+                    // Decide left or right
+                    if(transform.position.x < currentXs[i])
+                    {
+                        // walks RIGHT until it reaches the X_i position
+                        currentDir[i] = 2;
+                        if (transform.position.x >= currentXs[i])
+                        {
+                            canMove = false;
+                            i++;
+                        }
+                    } else {
+                        currentDir[i] = 4;
+                        // walks LEFT until it reaches the X_i position
+                        if (transform.position.x <= currentXs[i])
+                        {
+                            canMove = false;
+                            i++;
+                        }
+                    }
+                    
+                }
+                else if (currentYs[i] != 0)
+                {
+                    // Decide up or down
+                    if (transform.position.y < currentYs[i])
+                    {
+                        // walks UP until it reaches the Y_i position
+                        currentDir[i] = 1;
+                        if (transform.position.y >= currentYs[i])
+                        {
+                            canMove = false;
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        currentDir[i] = 3;
+                        // walks DOWN until it reaches the Y_i position
+                        if (transform.position.y <= currentYs[i])
+                        {
+                            canMove = false;
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    print("Should not have a value for both x_i & y_i");
+                }
+            }
+          
+        // The actual movement
+        isMoving = false;
+        moveVelocityX = 0;
+        moveVelocityY = 0;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocityX, moveVelocityY);
+        if (canMove)
+        {
+            isMoving = true;
+            if (currentDir[i] == 1)
+            {
+                moveVelocityY = walkSpeed * 3;
+            }
+            if (currentDir[i] == 3)
+            {
+                moveVelocityY = -walkSpeed * 3;
+
+            }
+
+            if (currentDir[i] == 2)
+            {
+                moveVelocityX = -walkSpeed * 4;
+
+            }
+
+            if (currentDir[i] == 4)
+            {
+                moveVelocityX = walkSpeed * 4;
+
+            }
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocityX, moveVelocityY) * Time.deltaTime;
+        }
+        animator.SetBool("IsMoving", isMoving);
+        animator.SetInteger("WalkDirection", currentDir[i]);
+        }
+    }
+}
