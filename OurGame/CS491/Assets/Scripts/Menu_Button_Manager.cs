acfc1088f11cb0b39927de00e4765e1c539a9589
Christopher <trojancj@uwec.edu>
2017-04-04 08:20:51 -0500

    Fixed menu script and re-added banner for it

---
+++ OurGame/CS491/Assets/Scripts/Menu_Button_Manager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu_Button_Manager : MonoBehaviour {
+	public void NewGameBtn(string newGameLevel){
+		SceneManager.LoadScene (newGameLevel);
+	}
+}
