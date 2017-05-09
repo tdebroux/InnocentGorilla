95faf9d2426856d175b2633007cd865821652438
Tommy <debroutp0968@uwec.edu>
2017-05-08 21:18:58 -0500

    SceneWA

---
+++ OurGame/CS491/Assets/Scripts/BridgeToFestivalTele.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BridgeToFestivalTele : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "MainCharacter")
+        {
+            SceneManager.LoadScene("SceneWA2b");
+        }
+    }
+}
